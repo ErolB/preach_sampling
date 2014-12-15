f559f64ffaf8e93e254e3dffd50ffbba10289535
Haitham Gabr <hgabr@cise.ufl.edu>
2014-12-15 17:10:09 -0500

    timing script for probing

--- data/time_probing.rb
+++ data/time_probing.rb
@@ -10,7 +10,7 @@ probe_start.step probe_end, probe_step do |probe_size|
 	time = 0.0
 	repeat.times do
 		puts "../preach #{dataset}/network.txt #{dataset}/sources.txt #{dataset}/targets.txt #{sample_prob} #{sample_size} #{method} #{probe_size}"
-		time = time + `(time -p ../preach #{dataset}/network.txt #{dataset}/sources.txt #{dataset}/targets.txt #{sample_prob} #{sample_size} #{method} #{probe_size}) 2>&1 | grep real | tr -d '\n' | cut -d" " -f2`.strip
+		time = time + `(time -p ../preach #{dataset}/network.txt #{dataset}/sources.txt #{dataset}/targets.txt #{sample_prob} #{sample_size} #{method} #{probe_size}) 2>&1 | grep real | tr -d '\n' | cut -d" " -f2`.strip.to_f
 	end
 	
 	open("#{output}/times_#{method}_#{sample_prob}_#{sample_size}.out", "a"){|f| f.puts "#{probe_size}\t\t#{time/repeat}"}
@@ -19,6 +19,6 @@ end
 time = 0.0
 repeat.times do
 	puts "../preach #{dataset}/network.txt #{dataset}/sources.txt #{dataset}/targets.txt #{sample_prob} #{sample_size} rand 10"
-	time = time + `(time -p ../preach #{dataset}/network.txt #{dataset}/sources.txt #{dataset}/targets.txt #{sample_prob} #{sample_size} rand 10) 2>&1 | grep real | tr -d '\n' | cut -d" " -f2`.strip	
+	time = time + `(time -p ../preach #{dataset}/network.txt #{dataset}/sources.txt #{dataset}/targets.txt #{sample_prob} #{sample_size} rand 10) 2>&1 | grep real | tr -d '\n' | cut -d" " -f2`.strip.to_f
 end
 open("#{output}/times_rand_#{sample_prob}_#{sample_size}.out", "w"){|f| f.puts "#{probe_start}\t\t#{time}\n#{probe_end}\t\t#{time/repeat}"}
