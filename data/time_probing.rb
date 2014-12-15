236dc3f2ec200ad7c6433e0b60a012415bbf95e4
Haitham Gabr <hgabr@cise.ufl.edu>
2014-12-15 16:57:23 -0500

    timing script for probing

---
+++ data/time_probing.rb
@@ -0,0 +1,16 @@
+#This script times a given probing method for several probe sizes against the single time consumed by the rand method
+dataset, output, sample_prob, sample_size, method, probe_start, probe_end, probe_step = ARGV
+probe_start = probe_start.to_i
+probe_end = probe_end.to_i
+probe_step = probe_step.to_i
+
+open("#{output}/times_#{method}_#{sample_prob}_#{sample_size}.out", "w"){|f| f.puts "#probe_size\ttime"}
+probe_start.step probe_end, probe_step do |probe_size|
+	puts "../preach #{dataset}/network.txt #{dataset}/sources.txt #{dataset}/targets.txt #{sample_prob} #{sample_size} #{method} #{probe_size}"
+	time = `(time -p ../preach #{dataset}/network.txt #{dataset}/sources.txt #{dataset}/targets.txt #{sample_prob} #{sample_size} #{method} #{probe_size}) 2>&1 | grep real | tr -d '\n' | cut -d" " -f2`.strip
+	open("#{output}/times_#{method}_#{sample_prob}_#{sample_size}.out", "a"){|f| f.puts "#{probe_size}\t\t#{time}"}
+end
+
+puts "../preach #{dataset}/network.txt #{dataset}/sources.txt #{dataset}/targets.txt #{sample_prob} #{sample_size} rand 10"
+time = `(time -p ../preach #{dataset}/network.txt #{dataset}/sources.txt #{dataset}/targets.txt #{sample_prob} #{sample_size} rand 10) 2>&1 | grep real | tr -d '\n' | cut -d" " -f2`.strip	
+open("#{output}/times_rand_#{sample_prob}_#{sample_size}.out", "w"){|f.puts "#{probe_start}\t\t#{time}\n#{probe_end}\t\t#{time}"|}
