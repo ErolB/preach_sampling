d7ec69ebb148840bcfb3ed426f1d0715f270ce3a
eroljb <eroljb@birchfinance.com>
2016-05-21 22:33:54 -0400

    edge shuffling, horizontial cuts in progress

--- preach.h
+++ preach.h
@@ -33,9 +33,6 @@
 #define MAX_EDGES 1024
 #define SURE 1.0
 
-#ifndef PREACH_H
-#define PREACH_H
-
 using namespace std;
 
 typedef bitset<MAX_NODES> Nodes_T;
@@ -84,6 +81,27 @@ double getCPUTime(){
 // macro to smoth up the use of bitsets
 #define FOREACH_BS(v, vSet)	  \
 	for (size_t v=vSet._Find_first(); v!=vSet.size(); v=vSet._Find_next(v))
+
+/*A class modeling a vertex cut*/
+class Cut{
+    Nodes_T middle; // The nodes in the
+    Nodes_T left; // Set of nodes on the left
+    Nodes_T right; // Set of nodes on the right
+    Edges_T coveredEdges; // Set of edges covered by this cut (left and middle)
+
+public:
+    Cut(){}
+
+    // consturctor for a specified cut
+	Cut(Nodes_T& _left, Nodes_T& _middle, Nodes_T& _right, Edges_T& _covered):
+		left(_left), middle(_middle), right(_right), coveredEdges(_covered){}
+
+    Nodes_T& getMiddle(){return middle;}
+    Nodes_T& getRight(){return right;}
+    Nodes_T& getLeft(){return left;}
+    Edges_T& getCoveredEdges(){return coveredEdges;}
+    int size(){return middle.count();}
+};
 
 class Term{
     Nodes_T z; // Reachable nodes
@@ -199,6 +217,9 @@ class Polynomial{
 public:
     Polynomial(vector<Term>& _terms): terms(_terms){}
 
+    // returns size of polynomial
+    int getSize(){ return terms.size(); }
+
     /*Adds and edge: mutiplies the whole polynomial by
     pX_subscript + (1-p)Y_subscript
     DOES NOT COLLAPSE AUTOMATICALLY*/
@@ -297,8 +318,6 @@ public:
         newTerms.swap(terms); //replace terms with the new collapsed terms
     }
 };
-
-#endif
 
 
 
