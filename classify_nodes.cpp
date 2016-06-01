7bb8bf306a709800fd38ed37aa038a00e7905bb2
eroljb <eroljb@birchfinance.com>
2016-05-31 20:47:13 -0400

    updated classifier for visualization tool

--- classify_nodes.cpp
+++ classify_nodes.cpp
@@ -1,23 +1,23 @@
 #include <stdio.h>
+#define DEBUG
 #ifdef DEBUG
+bool print_cuts = true;
 #define dprintf(fmt, ...) printf(fmt, ##__VA_ARGS__)
 #else
+bool print_cuts = false;
 #define dprintf(fmt, ...)
 #endif
 
 #include "Cut.h"
 
 string output_path = "/home/erol/Documents/preach_sampling/data.txt";  // path of output file
-char graph_file[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_4_85_2.txt";  // path of graph file
-char sourcesFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_4_85_2.sources"; // path of sources file
-char targetsFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_4_85_2.targets";  // path of targets file
 
 // returns a vector of the indicies of all ones
 vector<int> cvtBitset(Nodes_T input){
     vector<int> positions;
     for (int i = 0; i < input.size(); i++){
         if (input[i]){
-            positions.push_back(i);
+            positions.push_back(i+1);
         }
     }
     return positions;
@@ -25,39 +25,64 @@ vector<int> cvtBitset(Nodes_T input){
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////// main function
 
-int main(){
+int main(int argc, char** argv) {
     ListDigraph gOrig;
-    WeightMap wMapOrig(gOrig); // keeps track of weights
-    NodeNames nNames(gOrig); // node names
     NameToNode nodeMap; // mapping from names to nodes in the graph
-    ArcIntMap arcIdMapOrig(gOrig); // mapping from the arcs to their ids in the original graph
+    ListDigraph::Node sourceOrig; // start node
+    ListDigraph::Node targetOrig; // target node
 
-    CreateGraph(graph_file, gOrig, nNames, nodeMap, wMapOrig, arcIdMapOrig);
-    UnifyTerminals(gOrig, wMapOrig, nNames, nodeMap, arcIdMapOrig, sourcesFile, targetsFile);
-    ListDigraph::Node sourceOrig = FindNode(SOURCE, gOrig, nNames, nodeMap);
-    ListDigraph::Node targetOrig = FindNode(SINK, gOrig, nNames, nodeMap);
+    try {
+        char* graph_file = argv[1];  // path of graph file
+        char* sourcesFile = argv[2]; // path of sources file
+        char* targetsFile = argv[3];  // path of targets file
+
+        WeightMap wMapOrig(gOrig); // keeps track of weights
+        NodeNames nNames(gOrig); // node names
+        ArcIntMap arcIdMapOrig(gOrig); // mapping from the arcs to their ids in the original graph
+
+        CreateGraph(graph_file, gOrig, nNames, nodeMap, wMapOrig, arcIdMapOrig);
+        UnifyTerminals(gOrig, wMapOrig, nNames, nodeMap, arcIdMapOrig, sourcesFile, targetsFile);
+        sourceOrig = FindNode(SOURCE, gOrig, nNames, nodeMap);
+        targetOrig = FindNode(SINK, gOrig, nNames, nodeMap);
+    } catch (runtime_error) {
+        cout << "ERROR: setup failed";
+        exit(1);
+    }
 
     vector<Cut> cuts;
     // assigns a range of cuts to each node
-    vector< pair< int, vector< int > > > output;
-    vector< pair< vector< int >, int > > edgeSubsets;
+    vector<pair<int, vector<int> > > output;
+    vector<pair<vector<int>, int> > edgeSubsets;
     FindSomeGoodCuts(gOrig, sourceOrig, targetOrig, cuts, edgeSubsets);
     // iterate over every node
     for (ListDigraph::NodeIt n(gOrig); n != INVALID; ++n) {
-        int current_node = gOrig.id(n);
+        int current_node = gOrig.id(n);  // index must be shifted
         vector<int> cut_range;  // cuts that the node is a part of
         //iterate over every cut
-        for (int i = 1; i <= cuts.size(); ++i) {
-            vector<int> middle = cvtBitset(cuts[i - 1].getMiddle());  // nodes on current cut
+        for (int i = 0; i < cuts.size(); i++) {
+            vector<int> middle = cvtBitset(cuts[i].getMiddle());  // nodes on current cut
             // iterate over every node on the cut
-            for (int j = 0; j <= middle.size(); ++j) {
+            for (int j = 0; j < middle.size(); j++) {
                 // check if current node is on the cut
                 if (current_node == middle[j]) {
-                    cut_range.push_back(i);
+                    cut_range.push_back(i+1);
                 }
             }
         }
         output.push_back(make_pair(current_node, cut_range));
+    }
+
+    // print out cuts
+    cout << cuts.size() << endl;
+    if (print_cuts) {
+        for (int i = 0; i < cuts.size(); i++) {
+            cout << (i+1) << ": ";  // convert to 1-based indexing
+            vector<int> middle = cvtBitset(cuts[i].getMiddle());
+            for (int j = 0; j < middle.size(); ++j) {
+                cout << middle[j] << " ";
+            }
+            cout << endl;
+        }
     }
 
     // write data to file
@@ -69,7 +94,7 @@ int main(){
             if (output[i].second.size() > 0) {
                 data_file << output[i].first;
                 for (int j = 0; j < output[i].second.size(); j++) {
-                    data_file << ',' << output[i].second[j] + 1;
+                    data_file << ',' << output[i].second[j];
                 }
                 data_file << endl;
             }
