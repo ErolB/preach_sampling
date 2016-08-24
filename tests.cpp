df68ac7a9ea838af7be11b821581c2e0eab80499
eroljb <eroljb@birchfinance.com>
2016-08-24 13:21:15 -0400

    bug fix

--- tests.cpp
+++ tests.cpp
@@ -3,6 +3,21 @@
 #include "/usr/include/gtest/gtest.h"
 #include "Probing.h"  // includes all other PReach files
 
+/*
+// create function that builds graph structures
+ListDigraph& buildGraph(char graphFile[], char sourcesFile[], char targetsFile[]){
+    // create graph structure
+    ListDigraph g;
+    NameToNode nodeMap; // mapping from names to nodes in the graph
+    ListDigraph::Node source; // start node
+    ListDigraph::Node target; // target node
+    WeightMap wMap(g); // keeps track of weights
+    NodeNames nNames(g); // node names
+    ArcIntMap arcIdMap(g); // mapping from the arcs to their ids in the original graph
+    CreateGraph(graphFile, g, nNames, nodeMap, wMap, arcIdMap);
+}
+ */
+
 // verify that graph was properly created
 
 bool createGraphTest(char graph_file[]){
@@ -247,6 +262,202 @@ bool testIteration(char graphFile[], char sourcesFile[], char targetsFile[]){
     }
 }
 
+// tests the path finding function
+bool testPaths(char graphFile[], char sourcesFile[], char targetsFile[]){
+    ListDigraph gOrig;
+    NameToNode nodeMap; // mapping from names to nodes in the graph
+    ListDigraph::Node sourceOrig; // start node
+    ListDigraph::Node targetOrig; // target node
+    WeightMap wMapOrig(gOrig); // keeps track of weights
+    NodeNames nNames(gOrig); // node names
+    ArcIntMap arcIdMapOrig(gOrig); // mapping from the arcs to their ids in the original graph
+
+    CreateGraph(graphFile, gOrig, nNames, nodeMap, wMapOrig, arcIdMapOrig);
+    UnifyTerminals(gOrig, wMapOrig, nNames, nodeMap, arcIdMapOrig, sourcesFile, targetsFile);
+    sourceOrig = FindNode(SOURCE, gOrig, nNames, nodeMap);
+    targetOrig = FindNode(SINK, gOrig, nNames, nodeMap);
+
+    vector<Cut> cuts;
+    vector<pair<vector<int>, int> > edgeSubsets;
+    FindSomeGoodCuts(gOrig, sourceOrig, targetOrig, cuts, edgeSubsets);
+
+    bool success = true;
+    for (int i = 1; i < cuts.size(); i++){
+        for (int startNode: cvtBitset(cuts[i-1].getMiddle())){
+            vector<int> base = {startNode};
+            vector< vector<int> > paths = PathsFromPointTesting(gOrig, startNode, cvtBitset(cuts[i-1].getMiddle()), cvtBitset(cuts[i].getMiddle()), base);
+            // validate paths
+            for (vector<int> path: paths){
+                /*
+                cout << "nodes: ";
+                for (int node: path){ cout << node << ", ";}
+                cout << endl;
+                */
+                for (int j = 0; j < path.size(); j++) {
+                    int nodeId = path[j];
+                    ListDigraph::Node currentNode = gOrig.nodeFromId(nodeId);
+                    if (j > 0){
+                        bool nodePresent = false;
+                        int prevNodeId = path[j-1];
+                        for (ListDigraph::InArcIt a(gOrig, currentNode); a != INVALID; ++a){
+                            if (gOrig.id(gOrig.source(a)) == prevNodeId){
+                                nodePresent = true;
+                                break;
+                            }
+                        }
+                        if (!nodePresent){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
+
+// test edge ordering heuristic
+
+vector<int> orderEdges(ListDigraph& g, Cut cut1, Cut cut2){
+    // find paths
+    vector<int> startNodes = cvtBitset(cut1.getMiddle());
+    vector<int> endNodes = cvtBitset(cut2.getMiddle());
+    vector<int> edgeIdList = cvtEdgeBitset(cut2.getCoveredEdges() & ~cut1.getCoveredEdges());
+    vector< vector<int> > paths = PathsFromCutTesting(g, startNodes, endNodes);
+    cout << "found paths" << endl;
+
+    // Build a dictionary of edgeId -> source and target node ids
+    // Will need it with each collapsation operation within this sausage
+    map< int, vector<int> > edgeTerminals;
+    map< pair< int, int >, int > reverseTerminalMap;
+    // construct map structures representing paths
+    vector< map< int, bool > > path_maps;
+    for (vector<int> path: paths){
+        map< int, bool > current_map;
+        for (int edgeId: edgeIdList){
+            current_map[edgeId] = false;
+        }
+        for (int i = 1; i < path.size(); i++){
+            int current_id = reverseTerminalMap[make_pair(path[i-1], path[i])];
+            current_map[current_id] = true;
+        }
+        path_maps.push_back(current_map);
+    }
+    cout << "built path structure" << endl;
+    // create data structure to store scoring information for each edge
+    // each edge is assigned a map which represents each path it is in, and separates the paths by how close they are
+    //     to being completed
+    map< int, map< int, int > > edgeScores;
+    for (int edgeId: edgeIdList) {
+        map< int, int > scoreMap;
+        // for each path
+        for (map < int, bool > path_map: path_maps){
+            // if current edge on current path
+            if (path_map[edgeId]){
+                // count number of edges left until path is completed
+                int counter = 0;
+                for (int current_edge: edgeIdList){
+                    if (path_map[current_edge]){ counter++; }
+                }
+                if (counter == 0) { continue; }
+                // if the score map contains an entry for that distance from completion
+                if (scoreMap.count(counter)) { scoreMap[counter]++; }
+                else { scoreMap[counter] = 1; }
+                //cout << "edge: " << edgeId << ", score: " << counter << endl;
+            }
+        }
+        edgeScores[edgeId] = scoreMap;
+    }
+    cout << "edges scored" << endl;
+    int stop_count = 10;
+    vector<int> tempEdgeList = edgeIdList;
+    edgeIdList.clear();
+    while (!tempEdgeList.empty()){
+        // find ideal edge ("greedy" approach)
+        vector<int> bestEdges;
+        for (int i = 1; i < stop_count; i++) { // "i" is how close a path is to completion
+            int maxScore = -1;
+            for (int edgeId: tempEdgeList) {
+                // create blank entries in score map where none exist
+                map<int, int> scoreMap = edgeScores[edgeId];
+                if (!scoreMap.count(i)) {
+                    edgeScores[edgeId][i] = 0;
+                    scoreMap[i] = 0;
+                }
+                //if (!scoreMap.count(i)) { continue; } // continue if score is not available
+                if (scoreMap[i] > maxScore) { maxScore = scoreMap[i]; }
+            }
+            if (maxScore == -1) { continue; }  // new
+            // now that the maximum score is known, find the ideal edges
+            bestEdges.clear();  // start over
+            for (int edgeId: tempEdgeList) {
+                if (!edgeScores[edgeId].count(i)) { continue; }  // prevents creation of blank entries
+                if (edgeScores[edgeId][i] == maxScore){ bestEdges.push_back(edgeId); }
+            }
+        }
+        cout << "found ideal edges" << endl;
+        cout << tempEdgeList.size() << endl;
+        int bestEdge = bestEdges[0];  // pick an ideal edge (arbitrary)
+        // add edge to list and remove from temporary list
+        edgeIdList.push_back(bestEdge);
+        for (int i = 0; i < tempEdgeList.size(); i++){
+            if (tempEdgeList[i] == bestEdge){
+                tempEdgeList.erase(tempEdgeList.begin()+i);
+                break;
+            }
+        }
+        // update path map
+        for (map< int, bool > path: path_maps){
+            path[bestEdge] = false;
+        }
+        // update score map (make a separate function)
+        for (int edgeId: edgeIdList) {
+            map< int, int > scoreMap;
+            // for each path
+            for (map < int, bool > path_map: path_maps){
+                // if current edge on current path
+                if (path_map[edgeId]){
+                    // count number of edges left until path is completed
+                    int counter = 0;
+                    for (int current_edge: edgeIdList){
+                        if (path_map[current_edge]){ counter++; }
+                    }
+                    // if the score map contains an entry for that distance from completion
+                    if (scoreMap.count(counter) != 0) { scoreMap[counter]++; }
+                    else { scoreMap[counter] = 1; }
+                }
+            }
+            edgeScores[edgeId] = scoreMap;
+        }
+    }
+    return edgeIdList;
+}
+
+bool testHeuristic(char graphFile[], char sourcesFile[], char targetsFile[]){
+    // create graph structure
+    ListDigraph g;
+    NameToNode nodeMap; // mapping from names to nodes in the graph
+    ListDigraph::Node source; // start node
+    ListDigraph::Node target; // target node
+    WeightMap wMap(g); // keeps track of weights
+    NodeNames nNames(g); // node names
+    ArcIntMap arcIdMap(g); // mapping from the arcs to their ids in the original graph
+    CreateGraph(graphFile, g, nNames, nodeMap, wMap, arcIdMap);
+    UnifyTerminals(g, wMap, nNames, nodeMap, arcIdMap, sourcesFile, targetsFile);
+    source = FindNode(SOURCE, g, nNames, nodeMap);
+    target = FindNode(SINK, g, nNames, nodeMap);
+    vector<pair<vector<int>, int> > edgeSubsets;
+    vector<Cut> cuts;
+    FindSomeGoodCuts(g, source, target, cuts, edgeSubsets);
+    for (int node: cvtBitset(cuts[1].getMiddle())){
+        cout << node << endl;
+    }
+    vector<int> optimized = orderEdges(g, cuts[1], cuts[2]);
+    for (int node: optimized){
+        cout << node << endl;
+    }
+}
+
 TEST(GraphTest, small){
     char graph_file[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_5_2.txt";
     EXPECT_TRUE(createGraphTest(graph_file));
@@ -289,7 +500,7 @@ TEST(CutTest, large){
     char targetsFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_4_35_2.targets";
     EXPECT_TRUE(testCuts(graphFile, sourcesFile, targetsFile));
 }
-
+/*
 TEST(SolveTest, small){
     char graphFile[] = "/home/erol/Documents/preach_sampling/test_graphs/1.txt";
     char sourcesFile[] = "/home/erol/Documents/preach_sampling/test_graphs/1.sources";
@@ -304,6 +515,7 @@ TEST(SolveTest, small){
     EXPECT_TRUE(correct);
 }
 
+
 TEST(CollapseTest, should_collapse){
     char graph_file[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_5_4.txt";
     vector<int> present = {0, 1, 2, 3, 4, 5};
@@ -311,12 +523,27 @@ TEST(CollapseTest, should_collapse){
     bool result = testCollapsation(graph_file, present, ends);
     EXPECT_TRUE(result);
 }
+ */
 
 TEST(IterationTest, test){
     char graphFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_5_2.txt";
     char sourcesFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_5_2.sources";
     char targetsFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_5_2.targets";
     EXPECT_TRUE(testIteration(graphFile, sourcesFile, targetsFile));
+}
+
+TEST(PathTest, test){
+    char graphFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_10_4.txt";
+    char sourcesFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_10_4.sources";
+    char targetsFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_10_4.targets";
+    EXPECT_TRUE(testPaths(graphFile, sourcesFile, targetsFile));
+}
+
+TEST(HeuristicTest, test){
+    char graphFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_10_4.txt";
+    char sourcesFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_10_4.sources";
+    char targetsFile[] = "/home/erol/Documents/preach_sampling/data/synthetic/BA_2_10_4.targets";
+    testHeuristic(graphFile, sourcesFile, targetsFile);
 }
 
 int main(int argc, char** argv){
